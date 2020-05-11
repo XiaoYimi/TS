18e621f542e494533b85992cdebd0c8d6f0c731c
chenfengbukeyimi <2590856083@qq.com>
2020-05-11 13:03:26 +0800

    update webpack.config.js

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -4,6 +4,9 @@ const baseConfig = require('./webpack.base.config')
 const devConfig = require('./webpack.dev.config')
 const proConfig = require('./webpack.pro.config')
 
-let config = process.NODE_ENV === 'development' ? devConfig : proConfig
+// let config = process.env.NODE_ENV === 'development' ? devConfig : proConfig
 
-module.exports = merge(baseConfig, config)
+module.exports = (env, args) => {
+  let config = args.mode === 'development' ? devConfig : proConfig
+  return merge(baseConfig, config)
+}
